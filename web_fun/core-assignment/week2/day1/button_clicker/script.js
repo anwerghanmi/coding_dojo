5b8c79d5e07a7a61942272f26e82d93c4d88b4b0
anwerghanmi <ghanmianwer1@gmail.com>
2022-10-17 17:00:14 +0100

    button clicker

---
+++ web_fun/core-assignment/week2/day1/button_clicker/script.js
@@ -0,0 +1,9 @@
+function change(element) {
+    element.innerText = "logout";
+}
+function hide(element) {
+    element.remove();
+}
+function alerts(element) {
+    alert("ninja was liked", element);
+}
