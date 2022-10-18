0427c80383f850c6c2fb62a7e9ed0c542bd05f1c
anwerghanmi <ghanmianwer1@gmail.com>
2022-10-18 13:19:37 +0100

    likes

--- web_fun/core-assignment/week2/day1/likes/script.js
+++ web_fun/core-assignment/week2/day1/likes/script.js
@@ -1,5 +1,25 @@
-var count=3
-function plusone(){
+// function plusOne (element){
+//     var i=document.querySelector("add")
+//     i++
+// }
+// var elementnum= document.querySelector(".add")
+var count=9
+var count1=12
+var count2=9
+
+function plusOne (){
     count++
-    document.querySelector(".add").innerText=count +" "+ "like(s)"
+    document.querySelector(".add").innerText=count +" "+ "likes"
 }
+
+function plusOne1 (){
+    count1++
+    document.querySelector(".add1").innerText=count1 +" "+ "likes"
+}
+function plusOne2 (){
+    count2++
+    document.querySelector(".add2").innerText=count2 +" "+ "likes"
+}
+
+
+
